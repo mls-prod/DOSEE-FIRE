3a996d7fd10234c1412d2acf2c9870c4b982e78b
Wilfrieddevcreate <yves.heloussato@epitech.eu>
2024-09-28 17:39:19 +0100

    initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,17 @@
+export const images = {
+    logo: '/loader.webp', // if using local images
+    tapImage: "/maxitap.webp",
+    loadingScreen: '/loading.webp', // if using URLs
+  };
+  
+  export const texts = {
+    projectName: "MAXITAP",
+    projectSymbol: "MAX",
+  };
+
+  export const urls = {
+    appUrl: "https://maxitap.netlify.app",
+    botUsername: "maxitapbot",
+
+  }
+  
