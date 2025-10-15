3066b2022a9c557331e43024e9000d8feff900b0
MLS PRODUCTION <melisovim@icloud.com>
2025-10-15 23:47:37 +0300

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -10,7 +10,7 @@ export const images = {
   };
 
   export const urls = {
-    appUrl: "https://dosee-fire.vercel.app/",
+    appUrl: "https://doseemls.vercel.app",
     botUsername: "hmstmlsbot",
 
   }
